61d5a176fb38cd9a980efcb9934930fb3899a8e8
DissidentDan <dissidentdan@gmail.com>
2016-03-26 17:58:45 -0700

    Initial population with Unity project

---
+++ Assets/DestroyThrownObject.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using NewtonVR;
+
+public class DestroyThrownObject : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public AudioClip DestructionSound;
+
+	// Use this for initialization
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "MovableObject" || other.GetComponent<NVRInteractableItem>())
+        {
+            Destroy(other.gameObject);
+            if (audioSource && DestructionSound)
+            {
+                audioSource.clip = DestructionSound;
+                audioSource.Play();
+            }
+        }
+	}
+}
---
+++ Assets/PickUpObject.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpObject : MonoBehaviour {
+
+	///private bool LookingAtObject;
+
+	// Use this for initialization
+	public Material HighlightMaterial;
+	public Material UnhighlightMaterial;
+
+	private GameObject HighlightedObject;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		CastRayForward ();
+		if (Input.GetMouseButtonDown (0)) {
+			if(HighlightedObject != null)
+				HighlightedObject.transform.parent = this.transform;
+		}
+
+		if(Input.GetMouseButtonUp(0)) {
+			if(HighlightedObject != null)
+				HighlightedObject.GetComponent<Rigidbody>().isKinematic = false;
+				HighlightedObject.transform.parent = null;
+		}
+	}
+
+	void CastRayForward(){
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		RaycastHit hit;	
+		if (Physics.Raycast (transform.position, fwd, out hit)) { 
+			if (hit.transform.gameObject.tag == "MovableObject") {
+				hit.transform.gameObject.GetComponent<Renderer> ().material = HighlightMaterial;
+				HighlightedObject = hit.transform.gameObject;
+			}
+			else {
+				if(HighlightedObject != null){
+					HighlightedObject.GetComponent<Renderer>().material = UnhighlightMaterial;
+				}
+			}
+		} 
+
+	}
+
+
+}
